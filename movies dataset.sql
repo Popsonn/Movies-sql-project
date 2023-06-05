37c9a4c2abf10dfa75c3fbb33ee975f5e0bc3bde
Deborah Popoola <115980537+Popsonn@users.noreply.github.com>
2023-06-05 15:28:28 +0100

    Add files via upload

---
+++ movies dataset.sql
@@ -0,0 +1,121 @@
+-- List down the managers’ names at each store with the full address of each property (street, district, city, country)
+SELECT s.staff_id,s.first_name,s.last_name,a.address,a.district,ci.city,co.country FROM movies.staff s
+LEFT JOIN movies.address a ON s.address_id = a.address_id
+LEFT JOIN movies.city ci ON a.city_id = ci.city_id
+LEFT JOIN movies.country co ON ci.country_id = co.country_id
+
+-- Provide list of each inventory item, including store_id, inventory_id, title, film’s rating, rental rate, and replacement cost
+SELECT i.inventory_id,s.store_id,f.title,f.rating,f.rental_rate,f.replacement_cost FROM movies.inventory i
+JOIN movies.store s ON i.store_id = s.store_id
+JOIN movies.film f ON i.film_id = f.film_id
+
+--Provide summary level overview of your inventory — how many inventory items you have with each rating at each store?
+WITH sub1 as
+(SELECT i.inventory_id,s.store_id,f.title,f.rating,f.rental_rate,f.replacement_cost FROM movies.inventory i
+JOIN movies.store s ON i.store_id = s.store_id
+JOIN movies.film f ON i.film_id = f.film_id
+)
+
+SELECT count(inventory_id),rating,store_id FROM sub1
+GROUP BY 2,3
+ORDER BY 1
+
+--Extract a table containing the number of films, average replacement cost, and total replacement cost — sliced by store and film category.
+SELECT s.store_id,fc.category_id,count(f.film_id),avg(replacement_cost),sum(replacement_cost) from movies.store s
+join movies.inventory i on s.store_id = i.store_id
+join movies.film f on i.film_id = f.film_id
+join movies.film_category fc on f.film_id = fc.film_id
+group by 1,2
+
+-- List of all customer names, which store they go to, active or inactive, full address (street address, city, and country)
+SELECT CONCAT(c.first_name,' ',c.last_name)as customer_name,c.active,c.store_id,CONCAT(a.address,' ',ci.city,' ',co.country)AS full_address FROM movies.store s
+JOIN movies.customer c ON s.store_id = c.store_id
+JOIN movies.address a ON c.address_id = a.address_id
+JOIN movies.city ci ON a.city_id = ci.city_id
+JOIN movies.country co ON ci.country_id = co.country_id
+
+--List of customer names, total lifetime rentals, sum of all payments collected — order on the total lifetime value, most valuable customers on top
+SELECT concat(c.first_name,' ',c.last_name),count(r.rental_id)as total_rentals,sum(p.amount)as total_payment from movies.customer c
+JOIN movies.rental r ON c.customer_id = r.customer_id
+JOIN movies.payment p ON r.rental_id = p.rental_id
+GROUP BY 1
+ORDER BY 2 desc
+
+-- List of advisor and investor names in one table. Indicate whether they are an investor or an advisor. If they are an investor, include the company they work with.
+CREATE TABLE combined_table (
+  full_name text,
+  investor_or_advisor text,
+  company_name text
+);
+
+INSERT INTO combined_table (full_name,investor_or_advisor,company_name)
+SELECT concat(a.first_name, ' ', a.last_name),'Advisor', NULL FROM movies.advisor a;
+
+INSERT INTO combined_table (full_name,investor_or_advisor,company_name)
+SELECT concat(i.first_name, ' ', i.last_name),'Investor',i.company_name FROM movies.investor i;
+
+SELECT * FROM combined_table
+
+-- List of advisor and investor names in one table. Indicate whether they are an investor or an advisor. If they are an investor, include the company they work with.
+SELECT concat(a.first_name, ' ', a.last_name)as name,
+'Advisor' as role,
+NULL as company_name
+FROM movies.advisor a 
+UNION ALL
+SELECT concat(i.first_name, ' ', i.last_name)as name,
+'Investor' as role,
+i.company_name as company_role
+FROM movies.investor i
+
+--of all actors with three types of awards,for what % of them do we carry a film?
+with sub1 as(select concat(aa.first_name,' ',aa.last_name)as actor_name,
+aa.awards,aa.actor_id,
+case 
+ when aa.awards = 'Emmy,Oscar,Tony' then '3'
+  WHEN aa.awards LIKE '%Emmy%' AND aa.awards LIKE '%Oscar%' THEN '2'
+    WHEN aa.awards LIKE '%Emmy%' AND aa.awards LIKE '%Tony%' THEN '2'
+    WHEN aa.awards LIKE '%Oscar%' AND aa.awards LIKE '%Tony%' THEN '2'
+ else '1' 
+END AS number_of_awards
+from movies.actor_award aa)
+
+select (count(distinct actor_name)::numeric * 100)/(select count(*)
+from sub1 where number_of_awards = '3')::numeric as percentage
+from sub1 join movies.film_actor fm using (actor_id)
+where number_of_awards = '3'
+
+--of all actors with two types of awards,for what % of them do we carry a film?
+with sub1 as(select concat(aa.first_name,' ',aa.last_name)as actor_name,
+aa.awards,aa.actor_id,
+case 
+ when aa.awards = 'Emmy,Oscar,Tony' then '3'
+  WHEN aa.awards LIKE '%Emmy%' AND aa.awards LIKE '%Oscar%' THEN '2'
+    WHEN aa.awards LIKE '%Emmy%' AND aa.awards LIKE '%Tony%' THEN '2'
+    WHEN aa.awards LIKE '%Oscar%' AND aa.awards LIKE '%Tony%' THEN '2'
+ else '1' 
+END AS number_of_awards
+from movies.actor_award aa)
+
+select (count(distinct actor_name)::numeric * 100)/(select count(*)
+from sub1 where number_of_awards = '2')::numeric as percentage
+from sub1 join movies.film_actor fm using (actor_id)
+where number_of_awards = '2'
+
+--of all actors with one type of award,for what % of them do we carry a film?
+with sub1 as(select concat(aa.first_name,' ',aa.last_name)as actor_name,
+aa.awards,aa.actor_id,
+case 
+ when aa.awards = 'Emmy,Oscar,Tony' then '3'
+  WHEN aa.awards LIKE '%Emmy%' AND aa.awards LIKE '%Oscar%' THEN '2'
+    WHEN aa.awards LIKE '%Emmy%' AND aa.awards LIKE '%Tony%' THEN '2'
+    WHEN aa.awards LIKE '%Oscar%' AND aa.awards LIKE '%Tony%' THEN '2'
+ else '1' 
+END AS number_of_awards
+from movies.actor_award aa)
+
+select (count(distinct actor_name)::numeric * 100)/(select count(*)
+from sub1 where number_of_awards = '1')::numeric as percentage
+from sub1 join movies.film_actor fm using (actor_id)
+where number_of_awards = '1'
+
+
